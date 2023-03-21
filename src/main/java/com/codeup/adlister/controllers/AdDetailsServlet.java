dfd756997c5d9b985194dcece9335c9cefcf3b31
saidshah <saidmshah93@gmail.com>
2023-03-21 11:58:11 -0500

    button detail

--- src/main/java/com/codeup/adlister/controllers/AdDetailsServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdDetailsServlet.java
@@ -13,10 +13,8 @@ import java.io.IOException;
 
 @WebServlet(name = "controllers.AdDetailsServlet", urlPatterns = "/adDetail")
 public class AdDetailsServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         long adId = Long.parseLong(request.getParameter("id"));
         request.setAttribute("ad", DaoFactory.getAdsDao().findById(adId));
         request.getRequestDispatcher("/WEB-INF/ads/adsDetails.jsp").forward(request, response);
